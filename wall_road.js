4546bc54408dcaa7b85cea60fc9b618e25fb9f1f
POON Chun Yuen Alexander <144444630+cyapoon@users.noreply.github.com>
2024-11-25 15:21:38 +0800

    Add files via upload

---
+++ wall_road.js
@@ -0,0 +1,58 @@
+const Wall1 = function(ctx,x,y){
+    const sequence = { x: 0, y:  0, width: 25, height: 25, count: 1, timing: 200, loop: false };
+
+    const sprite = Sprite(ctx,x,y);
+
+    sprite.setSequence(sequence)
+        .setScale(1)
+        .setShadowScale({ x: 0.75, y: 0.2 })
+        .useSheet("wall1.png");
+
+    return {
+        getXY: sprite.getXY,
+        setXY: sprite.setXY,
+        getBoundingBox: sprite.getBoundingBox,
+        draw: sprite.draw,
+        update: sprite.update
+    };
+        
+}
+const Wall2 = function(ctx,x,y){
+    const sequence = { x: 0, y:  0, width: 25, height: 25, count: 1, timing: 200, loop: false };
+
+    const sprite = Sprite(ctx,x,y);
+
+    sprite.setSequence(sequence)
+        .setScale(1)
+        .setShadowScale({ x: 0.75, y: 0.2 })
+        .useSheet("wall2.png");
+
+    return {
+        getXY: sprite.getXY,
+        setXY: sprite.setXY,
+        getBoundingBox: sprite.getBoundingBox,
+        draw: sprite.draw,
+        update: sprite.update
+    };
+        
+}
+
+const Road = function(ctx,x,y){
+    const sequence = { x: 0, y:  0, width: 25, height: 25, count: 1, timing: 200, loop: false };
+
+    const sprite = Sprite(ctx,x,y);
+
+    sprite.setSequence(sequence)
+        .setScale(1)
+        .setShadowScale({ x: 0.75, y: 0.2 })
+        .useSheet("road.png");
+
+    return {
+        getXY: sprite.getXY,
+        setXY: sprite.setXY,
+        getBoundingBox: sprite.getBoundingBox,
+        draw: sprite.draw,
+        update: sprite.update
+    };
+        
+}
